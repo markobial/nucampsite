98efc1034bbc30f078cdaf30005ed0622910eb41
markobial <laibokram@gmail.com>
2021-07-31 00:10:58 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $("#reserveButton").click(function(){
+        $("#modalButton").modal('show');
+    });
+});
+
+$(function() {
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+    });
+});
